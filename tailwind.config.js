69971a7941b1faab508826a28e928f958f648e3d
Gustavo <thevicio27@gmail.com>
2024-12-15 16:33:39 -0300

    feat: animacao para o VOZ E VIOLAO e refatorado config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,13 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {
-
+      colors: {
+        "eerie": "#191919",
+        "pakistan": "#173C1B",
+        "vista": "#97A8CE",
+        "timberwolf": "#D1D1D1"
+      }
     },
-    colors: {
-      "eerie": "#191919",
-      "pakistan": "#173C1B",
-      "vista": "#97A8CE",
-      "timberwolf": "#D1D1D1",
-      "white": "#FFF"
-    }
   },
   plugins: [],
 }
